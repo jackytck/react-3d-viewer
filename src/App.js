460988800be88c05e2e3024ef9b85f04744d3d94
Jacky Tang <jackytck@gmail.com>
2017-08-22 00:00:17 +0800

    :hankey: use Scene3DViewer in App

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import ContainerDimensions from 'react-container-dimensions'
+import Scene3DViewer from './viewer3d/viewer3d'
+import { State } from './models'
+// import logo from './logo.svg';
+import './App.css'
+
+const state = new State()
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <ContainerDimensions>
+        <Scene3DViewer state={state} />
+      </ContainerDimensions>
+    )
   }
 }
 
-export default App;
+export default App
